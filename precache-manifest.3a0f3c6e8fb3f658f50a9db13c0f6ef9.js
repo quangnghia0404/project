b4983151ffef8bff93f5514e4f799721271e0657
unknown <dangquangnghia1999@gmail.com>
2022-01-13 15:59:21 +0700

    Updates

---
+++ precache-manifest.3a0f3c6e8fb3f658f50a9db13c0f6ef9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71dc6acb712319ba232cabee5f6b46a",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "2649a8985fc30a19e7df",
+    "url": "/project/static/css/2.e9c16cc8.chunk.css"
+  },
+  {
+    "revision": "cf967b4e22cfa08742ef",
+    "url": "/project/static/css/main.63d5fea3.chunk.css"
+  },
+  {
+    "revision": "2649a8985fc30a19e7df",
+    "url": "/project/static/js/2.481196fe.chunk.js"
+  },
+  {
+    "revision": "51e9562b1e3990695d353260aa3e32e8",
+    "url": "/project/static/js/2.481196fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf967b4e22cfa08742ef",
+    "url": "/project/static/js/main.7fecf2ca.chunk.js"
+  },
+  {
+    "revision": "00779be5a75421897f44",
+    "url": "/project/static/js/runtime-main.102d06f0.js"
+  },
+  {
+    "revision": "5c2ce3626299c80cc3b07117c6d2f08b",
+    "url": "/project/static/media/abc.5c2ce362.jpg"
+  },
+  {
+    "revision": "27f828e8b267bb85d01e05394e03588c",
+    "url": "/project/static/media/bai.27f828e8.jpg"
+  },
+  {
+    "revision": "6be6a14d1953f4675b1916f86ab6c6d8",
+    "url": "/project/static/media/ban23.6be6a14d.jpg"
+  },
+  {
+    "revision": "c1194249040de1d95f8d55fb6eba37c0",
+    "url": "/project/static/media/bg.c1194249.jpg"
+  },
+  {
+    "revision": "6cadb49e2f2ceca83c0cfe364b3de328",
+    "url": "/project/static/media/card.6cadb49e.png"
+  },
+  {
+    "revision": "b45140ebc0ba6e444136fddaa2dcd04b",
+    "url": "/project/static/media/content.b45140eb.png"
+  },
+  {
+    "revision": "af5abf7d714a853c28c1b16543e4f18a",
+    "url": "/project/static/media/logo.af5abf7d.png"
+  },
+  {
+    "revision": "8b6293be740cd83ef302c2d3ffbd58d0",
+    "url": "/project/static/media/makert.8b6293be.jpg"
+  },
+  {
+    "revision": "f5646a4adfb7a58f0b6fc6fb6945b3c3",
+    "url": "/project/static/media/roadmapp.f5646a4a.jpg"
+  },
+  {
+    "revision": "42aacb6c13f1ba3327d2cc29d4e4a4f2",
+    "url": "/project/static/media/thebai.42aacb6c.jpg"
+  },
+  {
+    "revision": "9551277caaead5d94cfc3da432cb962a",
+    "url": "/project/static/media/token.9551277c.jpg"
+  },
+  {
+    "revision": "4d4f312f85adead2e55688d162068f9f",
+    "url": "/project/static/media/video.4d4f312f.png"
+  },
+  {
+    "revision": "9ebceed2f280dc0364d5fc1706704cc8",
+    "url": "/project/static/media/yu.9ebceed2.jpg"
+  },
+  {
+    "revision": "a6b7fd7273df754633e72243a037290a",
+    "url": "/project/static/media/yugi1.a6b7fd72.jpg"
+  },
+  {
+    "revision": "c1cd4b0144e9da5fa093583495f38707",
+    "url": "/project/static/media/yugi5.c1cd4b01.jpg"
+  }
+]);
